828980c483230d4a2b6f9ddb4bd9dbfc6b455f34
Julien Arbey <jarbey@talentsoft.com>
2018-02-18 22:43:54 +0100

    Start function update inventory

--- src/Server/WebSocketComponent.php
+++ src/Server/WebSocketComponent.php
@@ -17,6 +17,9 @@ use Ratchet\ConnectionInterface;
 use Ratchet\MessageComponentInterface;
 
 class WebSocketComponent implements MessageComponentInterface {
+	const INVENTORY_SCAN = 'inventory_scan';
+	const UPDATE_QTY = 'update_qty';
+
 	/** @var \SplObjectStorage */
 	private $clients;
 
@@ -55,39 +58,99 @@ class WebSocketComponent implements MessageComponentInterface {
 	public function onError(ConnectionInterface $conn, \Exception $e) {
 		$this->logger->info('An error has occurred: ' . $e->getMessage());
 		$conn->send('An error has occurred: ' . $e->getMessage());
-		//$conn->close();
 	}
 
 	public function onMessage(ConnectionInterface $from, $message) {
 		$last_message = null;
 		$this->logger->info('Message received : ' . $message);
 
+		// Try to decode message
 		$server_message = json_decode($message);
 		if ($server_message != null) {
-			if (isset($server_message->cip)) {
-				$this->logger->debug('Message has CIP : ' . $server_message->cip);
-				$scanned_product = $server_message;
-
-				$product = new Product($scanned_product->id, $scanned_product->cip, $scanned_product->name, $scanned_product->stock, $scanned_product->inventory);
-				$product = $this->product_repository->setOrUpdateProduct($product);
-
-				// TODO : Use JMS Serializer here
-				$last_message = $this->serializer->serialize($product, 'json', SerializationContext::create()->setGroups(['product']));
-				$this->logger->debug('Product to send : ' . $last_message);
-			} else {
-				$this->logger->debug('Message without CIP');
-				$last_message = $message;
-			}
-		}
-
-		if ($last_message != null) {
-			// Send message to all clients
-			$this->last_message = $last_message;
-			foreach ($this->clients as $client) {
-				if ($from !== $client) {
-					$client->send($last_message);
+			if (isset($server_message->action)) {
+				switch ($server_message->action) {
+					case self::INVENTORY_SCAN:
+						$this->broadcastMessage($this->processInventoryScan($server_message), $from, true);
+						break;
+					case self::UPDATE_QTY:
+						$this->broadcastMessage($this->processUpdateQuantity($server_message), $from, true);
+						break;
+					default:
+						// TODO : Manage error
+						break;
 				}
+			} else {
+				// TODO : Manage error
+			}
+		} else {
+			// TODO : Manage error
+		}
+	}
+
+	/**
+	 * @param $message
+	 * @return string
+	 */
+	private function processUpdateQuantity($message) {
+		if (isset($message->id) && isset($message->qty)) {
+			/** @var Product $product */
+			$product = $this->product_repository->find($message->id);
+			if ($product != null) {
+				$product->setStock($message->qty);
+
+				return $this->saveProductAndGetMessage($product);
+			} else {
+				// TODO : Manager error
 			}
 		}
 	}
+
+	/**
+	 * @param $message (json product)
+	 * @return null|string
+	 */
+	private function processInventoryScan($scanned_product) {
+		if (isset($scanned_product->cip)) {
+			// SCAN ACTION
+			$this->logger->debug('CIP received : ' . $scanned_product->cip);
+			return $this->saveProductAndGetMessage(new Product($scanned_product->id, $scanned_product->cip, $scanned_product->name, $scanned_product->stock, $scanned_product->inventory));
+		}
+
+		// TODO : Manage error
+		return null;
+	}
+
+	/**
+	 * @param $message
+	 * @param ConnectionInterface $source_connection
+	 * @param bool $remember_message
+	 */
+	private function broadcastMessage($message, ConnectionInterface $source_connection, $remember_message = false) {
+		if ($message != null) {
+			// Send message to all clients except the source_connection
+			foreach ($this->clients as $client) {
+				if ($source_connection !== $client) {
+					$client->send($message);
+				}
+			}
+		}
+
+		// if need to remember message to deliver it on new connection
+		if ($remember_message) {
+			$this->last_message = $remember_message;
+		}
+	}
+
+	/**
+	 * @param Product $product
+	 * @return string
+	 */
+	private function saveProductAndGetMessage(Product $product) {
+		$product = $this->product_repository->setOrUpdateProduct($product);
+
+		$broadcast_messaged = $this->serializer->serialize($product, 'json', SerializationContext::create()->setGroups(['product']));
+		$this->logger->debug('Message to send : ' . $broadcast_messaged);
+		return $broadcast_messaged;
+	}
+
 }
