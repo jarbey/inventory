155b79437a6975837f2949d29d0021cc5c54e827
Julien Arbey <jarbey@talentsoft.com>
2018-02-19 14:25:41 +0100

    Start function update inventory

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -7,27 +7,35 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class ProductRepository extends ServiceEntityRepository {
+	const MODE_INCREASE_INVENTORY = 1;
+	const MODE_UPDATE_STOCK = 2;
+
 	public function __construct(RegistryInterface $registry) {
 		parent::__construct($registry, Product::class);
 	}
 
 	/**
 	 * @param Product $product
-	 * @param bool $increase_inventory
+	 * @param int $mode
 	 * @return Product
 	 */
-	public function setOrUpdateProduct(Product $product, $increase_inventory = true) {
+	public function setOrUpdateProduct(Product $product, $mode = 0) {
 		/** @var Product $existing_product */
 		$existing_product = $this->find($product->getId());
 		if ($existing_product == null) {
 			$this->getEntityManager()->persist($product);
 			$this->getEntityManager()->flush($product);
 			return $product;
-		} else if ($increase_inventory) {
-			$existing_product->addInventory($product->getInventory());
+		} else {
+			if ($mode & self::MODE_INCREASE_INVENTORY) {
+				$existing_product->addInventory($product->getInventory());
+			} else if ($mode & self::MODE_UPDATE_STOCK) {
+				$existing_product->setStock($product->getStock());
+			}
+
 			$this->getEntityManager()->flush($existing_product);
+			return $existing_product;
 		}
-		return $existing_product;
 	}
 
 }
